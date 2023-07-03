1b64a77caee4cd5a6d16547f1ebba1095ec767c1
Zayon Nieves <zayon.nieves1@gmail.com>
2023-07-03 19:13:17 -0400

    added AboutMe webpage and media queries for adapting content to mobile devices

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import HomeContent from './components/HomeContent';
 import TechStack from './components/TechStack';
+import Title from './components/Title';
 
 function App() {
   return (
@@ -28,7 +29,26 @@ function App() {
       </div>
 
       <div className='flexbox-container aboutPage'>
-        
+
+        <div id='aboutPic'>
+          <p>*insert image here</p>
+        </div>
+
+        <div className='aboutText'>
+          <Title  text='About Me'/>
+
+          <p>
+            As a Junior Full-Stack Developer, I possess a strong passion for technology and an<br />
+            impressive arsenal encompassing both front-end and back-end development.<br />
+            My skillset consists of; HTML, CSS, JavaScript, TypeScript, and React for creating dynamic<br />
+            web-apps, websites, and user interfaces.<br />
+            On the back-end I use; Node.js, Express.js, SQL, as well as the .NET-stack (Visual Studio, C#, SQLServer)<br />
+            <br />
+            I love to learn new things, build, and collaborate on projects with other developers.
+          </p>
+        </div>
+
+
       </div>
     </div>
   );
